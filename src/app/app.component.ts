8d1283610c6cebc3412996074c5e1611e08bbbff
Agustín Casado <agu.e.casado@gmail.com>
2022-06-16 14:22:32 -0300

    instalamos PrimeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre:string = 'Agustin Casado';
+  valor:number = 1000;
+  obj = {
+    nombre: 'AgusCa'
+  }
+
+  mostrarNombre() {
+    console.log( this.nombre );
+    console.log( this.valor );
+    console.log( this.obj );
+  }
 }
