ff4987a939bd08e18c6ce913c9965a5bbfba827d
Agustín Casado <agu.e.casado@gmail.com>
2022-06-21 16:02:55 -0300

    rutas de la aplicacion

---
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numeros',
+  templateUrl: './numeros.component.html',
+  styleUrls: []
+})
+export class NumerosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
