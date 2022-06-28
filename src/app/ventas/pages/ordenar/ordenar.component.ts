09c66b7b458e318db744bfd5166f7933bf936b3b
Agustín Casado <agu.e.casado@gmail.com>
2022-06-28 10:17:19 -0300

    llenar primeTable

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -16,4 +17,32 @@ export class OrdenarComponent {
       this.toMayus = true;
     } 
   }
+
+  heroes:Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
 }
